73bd8ca8855887791be1d8eaf155afffc7fe0318
ASUS <layboe1111@gmail.com>
2018-01-15 14:21:49 +0700

    Upload Project

---
+++ jwt/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express() ; 
+
+app.get('/api' ,(req , res) => {
+	res.json({
+		message : 'API Page'
+	});
+});
+
+
+app.post('/api/post' ,verifyToken ,  (req , res) => {
+	jwt.verify(req.token , 'secretkey' , (err , authData) => {
+		if (err) {
+			res.sendStatus(403);
+		} else {
+			res.json({
+				message : 'Post created ...' , 
+				authData
+			});
+		}
+	});
+	
+});
+
+app.post('/api/login' , (req , res) => {
+	const user = {
+		id : 1 , 
+		username : 'boe' , 
+		email : 'boe@gmail.com'
+	}
+	jwt.sign({user}, 'secretkey' ,(err , token ) => {
+		res.json({
+			token : token
+		});
+	});
+});
+
+//verify Token 
+function verifyToken(req , res , next){
+		//Get Auth hearder Value
+		const bearerHeader = req.headers['authorization'];
+		if (typeof bearerHeader !== 'undefined') {
+			const bearer = bearerHeader.split(' ');
+			const bearerToken = bearer[1] ;
+			
+			req.token = bearerToken;
+			next(); 
+		}else {
+			res.sendStatus(403); 
+		}
+}
+app.listen(5000  , () => console.log('Server start on port 5000'));
